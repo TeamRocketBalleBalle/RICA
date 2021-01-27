4bc6d009c5020dc0785cf12b7e0be0470cc67e4b
Yash <vasu2013agg@gmail.com>
2021-01-27 14:56:02 +0530

    Test Run for Flask
    Test run 6

--- Back_End (Flask)/Test Run.py
+++ Back_End (Flask)/Test Run.py
@@ -5,6 +5,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello() -> str:
+    return 'hello World'
+
+
 @app.route('/entry')
 def landing_page() -> 'html':
     return render_template('index.html')
