7030c4e999b863d4ef3b6cc2f3062871743ca945
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-01-27 22:08:03 +0530

    added handler functions for "doctors" & "chemists" and added gifs to the 1st handler function

--- Back_End (Flask)/Test Run.py
+++ Back_End (Flask)/Test Run.py
@@ -17,7 +17,17 @@ def landing_page() -> 'html':
 
 @app.route('/patients')
 def patients_page() -> 'html':
-    return "<h1>Patients page</h1> <iframe style=\"border: none; \" src=\"https://cdn.discordapp.com/emojis/768874484429226004.gif?v=1\"></iframe>"
+    return "<h1>Patient's page</h1> <iframe style=\"border: none; \" src=\"https://cdn.discordapp.com/emojis/768874484429226004.gif?v=1\"></iframe>"
+
+
+@app.route('/doctors')
+def doctors_page() -> 'html':
+    return "<h1>Doctor's page</h1> <iframe style=\"border: none; \" src=\"https://cdn.discordapp.com/emojis/791837082237665300.gif?v=1\"></iframe>"
+
+
+@app.route('/chemists')
+def chemists_page() -> 'html':
+    return "<h1>Chemist's page</h1> <iframe style=\"border: none; \" src=\"https://cdn.discordapp.com/emojis/754250296980668516.gif?v=11\"></iframe>"
 
 
 app.run(debug=True)
