b737ddf7bf34e7edfc68019334058cf0b25748da
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-01-27 16:13:57 +0530

    added exception handling if flask not installed
    notifies the user if flask is installed or not.
     also, enabled debug mode in flask

--- Back_End (Flask)/Test Run.py
+++ Back_End (Flask)/Test Run.py
@@ -1,8 +1,13 @@
 import html
+try:
+    from flask import Flask, render_template
+except ModuleNotFoundError as e:
+    print(e)
+    print("Flask module not found. Is flask installed on your computer? \ntry running: \npython -m pip install flask")
+    quit()
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
+    
+app = Flask(__name__,static_folder="templates")
 
 
 @app.route('/entry')
