dfd3a71fbd915e148021f069158e3d379cdfbe55
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-01-29 20:43:52 +0530

    [UNTESTED CODE] added login page function will also authenticate user and redirect to their resp page

--- Back_End (Flask)/Test Run.py
+++ Back_End (Flask)/Test Run.py
@@ -1,7 +1,7 @@
 import html
 
 try:
-    from flask import Flask, render_template
+    from flask import Flask, render_template, request, redirect
 except ModuleNotFoundError as e:
     print(e)
     print("Flask module not found. Is flask installed on your computer? \ntry running: \npython -m pip install -r requirements.txt")
@@ -19,20 +19,39 @@ def landing_page() -> 'html':
 
 @app.route('/patients')
 def patients_page() -> 'html':
-    # return "<h1>Patient's page</h1> <iframe style=\"border: none; \" src=\"https://cdn.discordapp.com/emojis/768874484429226004.gif?v=1\"></iframe>"
     return render_template("template.html", page_title="Patients", gif_link="https://cdn.discordapp.com/emojis/768874484429226004.gif?v=1")
 
 
 @app.route('/doctors')
 def doctors_page() -> 'html':
-    # return "<h1>Doctor's page</h1> <iframe style=\"border: none; \" src=\"https://cdn.discordapp.com/emojis/791837082237665300.gif?v=1\"></iframe>"
     return render_template("template.html", page_title="Doctors", gif_link="https://cdn.discordapp.com/emojis/791837082237665300.gif?v=1")
 
 
 @app.route('/chemists')
 def chemists_page() -> 'html':
-    # return "<h1>Chemist's page</h1> <iframe style=\"border: none; \" src=\"https://cdn.discordapp.com/emojis/754250296980668516.gif?v=11\"></iframe>"
     return render_template("template.html", page_title="Chemists", gif_link="https://cdn.discordapp.com/emojis/754250296980668516.gif?v=11")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login_page() -> 'html':
+    """
+    if the userid and password are correct, the user is redirected to the resp page [ie, Patient's, Doctor's, Chemist's]
+    if they're not, then we redirect to the same sign-in page, with an error message popping up [bootstrap]
+    """
+    if request.form:
+        # --------- Verifying user's entry------------
+        import loginVerify
+
+        email = request.form['email']
+        password = request.form['password']
+
+        if loginVerify.login(email, password):
+            return redirect(f"/{loginVerify.get_user_class(email)}")
+        else:
+            return render_template("login.html", page_title="Login", login_failed=True)
+
+    else:
+        return render_template("login.html", page_title="Login", login_failed=False)
 
 
 app.run(debug=True)
