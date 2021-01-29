517812561a1512c92ad941481055196291abf4fb
Yash <vasu2013agg@gmail.com>
2021-01-29 20:26:55 +0530

    New Table created Profiles in Database....

--- Database/Table_Editor.py
+++ Database/Table_Editor.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect('Rica_AlphaV0.1.db')
 cur = conn.cursor()
-patient_entry = [(2,'SHR08OPXD','Shr@epic.com',3454389841,'Hyper Tension','Omicron','08/04/2004','Faltu','Shr88','testcase1'),(3,'MofoAkshat','Aks@epic.com',6584271395,'Covid Survivor','Delta Noida','04/08/2001','Faltu','MofoAks','testcase2')]
-cur.executemany("INSERT INTO Patients VALUES(?,?,?,?,?,?,?,?,?,?)",patient_entry)
+profile_list = [('ramgopal@123.com','Chemist'),('gabbarakshay!321.com','Chemist'),('jayant@456.com','Chemist'),('ysh@123.com','Patient'),('Shr@epic.com','Patient'),('Aks@epic.com','Patients')]
+cur.executemany("INSERT INTO Profiles VALUES(?,?)",profile_list)
 conn.commit()
 conn.close()
