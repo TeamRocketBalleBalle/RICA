8eb4a98cf73e14c8994343f9c4ef87e27811d13d
shreyanshsharma88 <77317713+shreyanshsharma88@users.noreply.github.com>
2021-01-29 17:55:24 +0530

    YASH KI JAGAH

--- Database/Table_Editor.py
+++ Database/Table_Editor.py
@@ -2,10 +2,29 @@ import sqlite3
 
 conn = sqlite3.connect('Rica_AlphaV0.1.db')
 cur = conn.cursor()
-patients_entry = [(1,'Ysh','ysh@123.com',1234567891,'nothing','Delhi','22/06/2003','Faltu','yash123','123456578')]
-cur.executemany("INSERT INTO Patients VALUES (?,?,?,?,?,?,?,?,?,?)",patients_entry)
-conn.commit()
-conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#DO NOT TOUCH
+#SHREYANSH KI LINE H YE
+
+
 chemvalues=[("Ram Gopal","ramgopal@123.com",9876543210,5,"Borawali East",1,"id.user",2021)
               ("Gabbar","gabbarakshay!321.com",8976543210,4,"Alpha 1",2,"user.id",2022)
               ("jayant","jayant@456.com",9384758393,3,"Gurugram",3,"test.did",2023)]
