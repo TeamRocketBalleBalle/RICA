1d77bb8d6cab397c52a1f8f48a7010b115be2613
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2021-01-30 13:45:08 +0530

    Did some voodoo magic [function decorator] to connect/close to the database #5 sucks, right @Ya-s-h?

--- Database/Database_connection.py
+++ Database/Database_connection.py
@@ -1,9 +1,31 @@
-import sqlite3, sys, os
+import os
+import sqlite3
 
-conn = sqlite3.connect('Rica_AlphaV0.1.db')
-cur = conn.cursor()
 
-def check_existence(em):
+def connect_and_close(func):
+    """
+    wrapper function to start and close the connection to database whenever the other functions are called.
+    :param func: function
+    :return: function output
+    """
+
+    def wrap(*args, **kwargs):
+        # connect to the database
+        conn = sqlite3.connect('Rica_AlphaV0.1.db')
+        cur = conn.cursor()
+        args += (cur,)  # give the cursor argument to the func
+        # call the function
+        result = func(*args, **kwargs)
+
+        # close the connection to database
+        conn.close()
+        return result
+
+    return wrap
+
+
+@connect_and_close
+def check_existence(em, cur):
     # os.chdir("../Database")
     print(f"databse_connection ke andar: {os.getcwd()}")
     # breakpoint()
@@ -11,14 +33,18 @@ def check_existence(em):
     return bool(cur.fetchall())
 
 
-def get_user_type(em):
+@connect_and_close
+def get_user_type(em, cur):
     cur.execute(f"SELECT Ptype FROM Profiles WHERE email = \"{em}\"")
     return cur.fetchall()[0][0]
 
 
-def get_password(em):
+@connect_and_close
+def get_password(em, cur):
     typ = get_user_type(em)
     cur.execute(f"SELECT pass from {typ}s WHERE email = \"{em}\"")
     return cur.fetchall()[0][0]
 
-print(check_existence("ysh@123.com"))
+
+if __name__ == "__main__":
+    print(check_existence("ysh@123.com"))
