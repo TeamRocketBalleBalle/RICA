eb6024847b67b408249097c417d043e7b35e3955
Yash <vasu2013agg@gmail.com>
2021-02-03 13:38:12 +0530

    Doctor.table now contain values....finally...uffff

--- Database/Table_Editor.py
+++ Database/Table_Editor.py
@@ -4,6 +4,6 @@ from farjidoctors import l1
 conn = sqlite3.connect('Rica_AlphaV0.1.db')
 cur = conn.cursor()
 # profile_list = [('Aks@epic.com','Patient')]
-cur.executemany()
+cur.executemany("INSERT INTO Doctors VALUES(?,?,?,?,?,?,?)",l1)
 conn.commit()
 conn.close()
