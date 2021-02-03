0c2db3484851012b538564ab4ff74bb4f6b8f3dc
Yash <vasu2013agg@gmail.com>
2021-02-03 10:36:31 +0530

    Data has been created for Doctors table... Waiting for json file then will add values to database

--- Database/Table_Editor.py
+++ Database/Table_Editor.py
@@ -1,17 +1,9 @@
 import sqlite3
+from farjidoctors import l1
 
 conn = sqlite3.connect('Rica_AlphaV0.1.db')
 cur = conn.cursor()
 # profile_list = [('Aks@epic.com','Patient')]
-cur.execute("DROP TABLE Doctors")
-cur.execute("""CREATE TABLE Doctors(
-    name Text , 
-    email BLOB,
-    phone no INTEGER,
-    specialization BLOB,
-    loc Text,
-    pass BLOB,
-    appointment BLOB
-)""")
+cur.executemany()
 conn.commit()
 conn.close()
