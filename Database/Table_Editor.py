4bed4c02f1d7bbcf035c68a17d4e4aabdc4becb9
Yash <vasu2013agg@gmail.com>
2021-02-07 15:20:53 +0530

    TODO: - https://github.com/TeamRocketBalleBalle/RICA/projects/1#card-54376215 Completed
    @RoguedBear

--- Database/Table_Editor.py
+++ Database/Table_Editor.py
@@ -1,9 +1,11 @@
 import sqlite3
-from farjidoctors import l1
+# from farjidoctors import l1
 
 conn = sqlite3.connect('Rica_AlphaV0.1.db')
 cur = conn.cursor()
 # profile_list = [('Aks@epic.com','Patient')]
-cur.executemany("INSERT INTO Doctors VALUES(?,?,?,?,?,?,?)",l1)
+cur.execute("ALTER TABLE Doctors ADD COLUMN UID BLOB")
+cur.execute("ALTER TABLE Patients ADD COLUMN Prescription BLOB")
+
 conn.commit()
 conn.close()
