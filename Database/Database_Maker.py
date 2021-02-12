790a3eb5654c0583da0b2284a4b69ac6179ce05f
Yash <vasu2013agg@gmail.com>
2021-02-12 10:02:02 +0530

    Change a column name of Profiles table

--- Database/Database_Maker.py
+++ Database/Database_Maker.py
@@ -7,7 +7,7 @@ cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS Doctors")
 cur.execute("DROP TABLE IF EXISTS Patients")
 cur.execute("DROP TABLE IF EXISTS Chemists")
-
+cur.execute("DROP TABLE IF EXISTS Profiles")
 # Making Table Doctors
 cur.execute("""CREATE TABLE Doctors(  
     name TEXT,
@@ -18,6 +18,7 @@ cur.execute("""CREATE TABLE Doctors(
     pass TEXT,
     appointment BLOB,
     UID INTEGER)""")
+# Making Patients Tables
 cur.execute("""CREATE TABLE Patients(
     reg_no INTEGER,  
     name TEXT,
@@ -30,6 +31,7 @@ cur.execute("""CREATE TABLE Patients(
     password TEXT,    
     Prescription BLOB)
     """)
+# Making Chemists Table
 cur.execute("""CREATE TABLE Chemists(
     Name TEXT,
     email TEXT,
@@ -39,5 +41,11 @@ cur.execute("""CREATE TABLE Chemists(
     UID INTEGER,
     password TEXT)
     """)
+# Making Profiles Table
+cur.execute("""CREATE TABLE Profiles(
+    email TEXT,
+    UType TEXT)
+""")
+print("EXECUTED SUCCESSFULLY")
 conn.commit()
 conn.close()
