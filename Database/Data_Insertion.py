bc90a18d31fe2fd7e4dccd436858db571aa30299
Yash <vasu2013agg@gmail.com>
2021-02-12 10:01:35 +0530

    Data For Profiles Table has been updated

--- Database/Data_Insertion.py
+++ Database/Data_Insertion.py
@@ -1,4 +1,5 @@
 import sqlite3
+import Database_Maker
 import Database_Data
 
 conn = sqlite3.connect('Rica_AlphaV0.1.db')
@@ -7,5 +8,6 @@ cur = conn.cursor()
 cur.executemany("INSERT INTO Doctors Values(?,?,?,?,?,?,?,?)", Database_Data.Doctors_Data)
 cur.executemany("INSERT INTO Patients Values(?,?,?,?,?,?,?,?,?,?)", Database_Data.Patients_Data)
 cur.executemany("INSERT INTO Chemists Values(?,?,?,?,?,?,?)", Database_Data.Chemists_Data)
+cur.executemany("INSERT INTO Profiles Values(?,?)", Database_Data.Profiles_Data)
 conn.commit()
 conn.close()
